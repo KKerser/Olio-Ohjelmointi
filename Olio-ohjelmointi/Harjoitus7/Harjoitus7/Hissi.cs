e12fefc62d8b8d520d837b432860051e84fe6d7c
Krister Kovalenko <Krister.kovalenko@student.careeria.fi>
2023-05-02 21:47:10 +0300

    Harjoitus7

---
+++ Olio-ohjelmointi/Harjoitus7/Harjoitus7/Hissi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus7
+{
+    internal class Hissi
+    {
+        private int kerros;
+
+        private int minKerros;
+        private int maxKerros;
+
+        public int Kerros
+        {
+            set
+            {
+                if (value < minKerros)
+                {
+                    Virhe.KirjoitaVirhe("Virheellinen kerros annettu.");
+                }
+                else if (value > maxKerros)
+                {
+                    Virhe.KirjoitaVirhe("Virheellinen kerros annettu.");
+                }
+                else
+                {
+                    kerros = value;
+                }
+            }
+        }
+
+        public Hissi(int _minKerros, int _maxKerros)
+        {
+            minKerros = _minKerros;
+            maxKerros = _maxKerros;
+        }
+
+        public void TulostaKerros()
+        {
+            Console.WriteLine("Nykyinen kerros on " + kerros);
+        }
+
+        public string PalautaMaxMin()
+        {
+            return minKerros + "-" + maxKerros;
+        }
+    }
+}
---
+++ Olio-ohjelmointi/Harjoitus7/Harjoitus7/Program.cs
@@ -0,0 +1,35 @@
+﻿//Krister Kovalenko
+//Program.cs - Hissi
+
+using Harjoitus7;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Hissi hissi = new Hissi(1, 6);
+
+
+        int uusiKerros = 0;
+
+        hissi.TulostaKerros();
+
+        while (true)
+        {
+            Console.WriteLine();
+            Console.Write("Anna uusi kerros 1-6: ", hissi.PalautaMaxMin());
+
+            bool tarkista = int.TryParse(Console.ReadLine(), out uusiKerros);
+
+            if (tarkista)
+            {
+                hissi.Kerros = uusiKerros;
+            }
+
+
+            hissi.TulostaKerros();
+        }
+
+
+    }
+}
